ea43e47395b4170ad0b0a01dacbf70f232521b4f
BikashPandeya <bikashpandeya5@gmail.com>
2025-06-02 21:05:04 +0545

    First problem

--- 1_Watermelon.cpp
+++ 1_Watermelon.cpp
@@ -1,31 +1,13 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-    long long int n;
-    cin >> n;
-    cout << n << " "; // Output the initial value of n
-    while (n > 0)
-    {
-        if (n == 1)
-        {
-            break;
-        }
-        if (n % 2 == 0)
-        {
-            
-            n=n/2;
-           cout << n << " ";
+int main(){
+    int w;
+    cin >> w;
+    if(w%2 == 0 && w > 2){
+        cout << "YES";
 
-        }
-        else
-        {
-
-           
-            n = n * 3 +1;
-            cout << n << " ";
-
-        }
+    }else{
+        cout << "NO";
     }
 }
