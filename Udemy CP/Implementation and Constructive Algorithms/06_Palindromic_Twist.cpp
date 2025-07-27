5ff246a9a3b709c8c54f9ff70b568d36cd8275ad
BikashPandeya <bikashpandeya5@gmail.com>
2025-07-27 22:44:42 +0545

    Solving 6th problem

---
+++ Udemy CP/Implementation and Constructive Algorithms/06_Palindromic_Twist.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    for(int i = 0 ; i < n ; i ++){
+        int k , count = 1;
+        string s;
+        cin >> k >> s;
+        string temp = s , s1 = s;;
+        char ch = s[k-1];
+        for(int i=0;i<k;i++){
+            if(s[i]-s[k-i-1] == 2 || s[i]-s[k-i-1] == -2){
+               continue;
+            }else if(s[i]-s[k-i-1] == 0){
+                continue;
+            }else{
+                count = 0;
+                break;
+            }
+        }
+        if(count == 1){
+            cout << "YES" << endl;
+        }else{
+            cout << "NO" << endl;
+        }
+    }
+    return 0;
+}
